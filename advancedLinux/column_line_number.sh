2f408ffd4c918ea8239837c96981300361af92f2
Carlos Cruz <carlos.cruzper@gmail.com>
2019-10-25 21:57:55 +0200

    last commit of advanced linux class

--- advancedLinux/column_line_number.sh
+++ advancedLinux/column_line_number.sh
@@ -1,2 +1,8 @@
-head -1 optd_aircraft.csv | tr "^" "\n" | wc -l
-paste <(seq 8) <(head -1 optd_aircraft.csv | tr "^" "\n")
+#!/bin/bash
+DELIMITER=$1
+FILE=$2
+echo 'Delimiter:'$DELIMITER 'File:'$FILE
+
+
+NUM_COLUMNS=$(head -1 $FILE | tr "$DELIMITER" "\n" | wc -l)
+paste <(seq $NUM_COLUMNS) <(head -1 $FILE | tr "$DELIMITER" "\n")
